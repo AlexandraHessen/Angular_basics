64420a70cbdac0e56e1f05f2b5169e3163c49c38
AlexandraHessen <xl_energy@mail.ru>
2022-06-01 01:06:05 +0300

    ContentChild

--- 2_block/src/app/post/post.component.ts
+++ 2_block/src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ContentChild, Input, OnInit, ElementRef } from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -9,13 +9,21 @@ import { Post } from '../app.component';
 export class PostComponent implements OnInit {
 
   @Input() post: Post
-  //post -  название переменной с которой будет ра
+  //post -  название переменной с которой будет работать
   //: Post -  тип Post, interface Post который мв описали в родит компоненте + импортируем его
   // @Input() - дикоратор который говорит что даннаая переменная post будет получена извне + импортируем его
+
+  @ContentChild('info', {static: true}) infoRef: ElementRef
+  //infoRef -  название переменной с которой будет работать
+  //: ElementRef -  ElementRef - тип : ElementRef - те переменные которые мы получаем с помощью декоратора ContentChild имеют специальный тип ElementRef  + испортируем
+  // @ContentChild('info' ) - обязательно передаем параметр info - это название нашей ссылки через #
+  // @ContentChild() - дикоратор  + импортируем его
+  // {static: true} если используем эту переменную в методе  ngOnInit() ставим true dj всех остальных случаях false
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.infoRef);
   }
 
 }
