8699a5b93cb642fcad4d878c68d5bc88bef6969c
AlexandraHessen <xl_energy@mail.ru>
2022-06-01 00:29:05 +0300

    work with HTML element

--- 2_block/src/app/post-form/post-form.component.ts
+++ 2_block/src/app/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-post-form',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostFormComponent implements OnInit {
 
+  @ViewChild('titleInput') inputRef: ElementRef
+  // @ViewChild - дикоратор  + испортируем
+  // @ViewChild('titleInput') - обязательно передаем параметр titleInput - это название нашей ссылки через #
+  // inputRef - переменная
+  // inputRef: ElementRef - тип : ElementRef - те переменные которые мы получаем с помощью декоратора ViewChild имеют специальный тип ElementRef  + испортируем
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  focusTitle(){
+    console.log(this.inputRef);
+    // this.inputRef.nativeElement =  тоже самое что мы получаем при document.qurySelector и все методы можем вызывать как обычно
+    this.inputRef.nativeElement.focus()
   }
 
 }
