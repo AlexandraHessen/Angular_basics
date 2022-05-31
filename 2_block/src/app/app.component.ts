60ce0e53c74cc26c6a7d58d189cbf12b087f132c
AlexandraHessen <xl_energy@mail.ru>
2022-06-01 01:38:44 +0300

    ChangeDetectionStrategy

--- 2_block/src/app/app.component.ts
+++ 2_block/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export interface Post{
   title: string
@@ -11,11 +11,25 @@ export interface Post{
   templateUrl: './app.component.html', //верстка компонента
   styleUrls: ['./app.component.scss'] //стили компонента
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   posts: Post[] =[         //Post[] тип - массив объектов класса Post
     {title: 'Хочу выучить ', text: 'Я все еще', id: 1},
-    {title: 'Следующий блок', text: 'Будет про', id: 2}
+    // {title: 'Следующий блок', text: 'Будет про', id: 2}
   ]
+
+  ngOnInit(): void {
+    setTimeout(()=>{
+      console.log("Timeout");
+      // this.posts[0].title = "change"
+      this.posts[0]={
+        title: "change",
+        text: "change",
+        id: 33
+      }
+    }, 5000
+
+    )
+  }
 
 
 }
