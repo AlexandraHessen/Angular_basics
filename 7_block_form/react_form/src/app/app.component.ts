86e26d18a7b55ecf9fdb548ec84319a18ae33b9b
AlexandraHessen <xl_energy@mail.ru>
2022-06-07 16:57:30 +0300

    add validators

--- 7_block_form/react_form/src/app/app.component.ts
+++ 7_block_form/react_form/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root', //тег в который будет загружаться компонент
   templateUrl: './app.component.html', //верстка компонента
   styleUrls: ['./app.component.scss'] //стили компонента
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup //переменная и ее тип
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      // email: new FormControl("xl@mail", Validators.email)
+      email: new FormControl("xl@mail", [Validators.email, Validators.required])
+      //Validators.minLength(6)
 
+    })
+  }
 
+  submit(){
+    console.log('Form sunmited: ', this.form);
+  }
 }
+
